802fc66ff31b44169d9c496ab1e6be8e1ec0bec6
Ankush Amar <ankushamar018@gmail.com>
2026-08-17 19:28:56 +0530

    17-aug-26

--- practice.js
+++ practice.js
@@ -3,11 +3,28 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.headers);
     res.write(`
-        
+        <html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <hread>
+        <nav>
+            <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/men">Men</a></li>
+                <li><a href="/women">Women</a></li>
+                <li><a href="/kids">Kids</a></li>
+                <li><a href="/cart">🛒</a></li>
+            </ul>
+        </nav>
+    </hread>
+</body>
+</html>
     `);   
 
 });
 
-server.listene(3000, () => {
+server.listen(3000, () => {
     console.log(`Server running on address http://localhost:3000`);
 });
